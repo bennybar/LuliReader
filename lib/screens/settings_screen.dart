05c92f3b8b7e1695739d5a6ae33e89df226bdd38
bbarak@kenes.com <bbarak@kenes.com>
2025-12-28 21:03:39 +0200

    1

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -925,7 +925,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
               ListTile(
                 leading: const Icon(Icons.info),
                 title: const Text('About'),
-                subtitle: const Text('Luli Reader v1.1.78'),
+                subtitle: const Text('Luli Reader v1.1.80'),
                 trailing: const Icon(Icons.chevron_right),
               ),
             ],
