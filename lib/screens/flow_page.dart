d671437de9259c07532ba09c444cf8aaed63b1e7
bbarak@kenes.com <bbarak@kenes.com>
2026-03-16 07:24:44 +0200

    1

--- lib/screens/flow_page.dart
+++ lib/screens/flow_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/services.dart';
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static const _fontFamily = 'GoogleSans';
+
   static ThemeData lightTheme(ColorScheme? colorScheme) {
     final scheme = colorScheme ?? ColorScheme.fromSeed(
       seedColor: Colors.blue,
       brightness: Brightness.light,
     );
+    final baseTextTheme = ThemeData(
+      useMaterial3: true,
+      colorScheme: scheme,
+      fontFamily: _fontFamily,
+    ).textTheme;
 
     return ThemeData(
       useMaterial3: true,
       colorScheme: scheme,
+      fontFamily: _fontFamily,
+      textTheme: baseTextTheme,
+      primaryTextTheme: baseTextTheme,
       cardTheme: CardThemeData(
         elevation: 0,
         shape: RoundedRectangleBorder(
@@ -29,10 +39,18 @@ class AppTheme {
       seedColor: Colors.blue,
       brightness: Brightness.dark,
     );
+    final baseTextTheme = ThemeData(
+      useMaterial3: true,
+      colorScheme: scheme,
+      fontFamily: _fontFamily,
+    ).textTheme;
 
     return ThemeData(
       useMaterial3: true,
       colorScheme: scheme,
+      fontFamily: _fontFamily,
+      textTheme: baseTextTheme,
+      primaryTextTheme: baseTextTheme,
       cardTheme: CardThemeData(
         elevation: 0,
         shape: RoundedRectangleBorder(
